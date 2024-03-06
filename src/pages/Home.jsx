1394e4a9a9667871b010badd37c25ac4006a6667
Matteo <matteodurso45@gmail.com>
2024-03-07 00:53:44 +0100

    modifica Body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
-import { Body } from "../content/Body";
+import { CarouselHome } from "../content/Carousel/CarouselHome";
 import { Header } from "../header/Header";
 
 export function Home() {
 	return (
 		<div>
 			<Header />
-			<Body />
+			<div className="flex justify-content-center	">
+			<div className="flex-3 h-4rem bg-primary font-bold text-center p-4 border-round">1</div>
+			<div className="flex-6 h-4rem text-center p-4 border-round mx-4">
+				<CarouselHome />
+			</div>
+			<div className="flex-3 h-4rem bg-primary font-bold text-center p-4 border-round">3</div>
+		</div>
 		</div>
 	);
 }
