0c091d3c458f0cab522e0f0f99086cd8807fe577
Matteo <matteodurso45@gmail.com>
2024-03-07 02:14:35 +0100

    modifiche

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Navigation } from "./Navigation.jsx";
-
+const current_theme = localStorage.getItem("current_theme")
+const [theme , setTheme]= useState(current_theme ? current_theme: "light")
+useEffect((
+localStorage
+),[theme])
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
