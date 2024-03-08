14231fcb239a50f518b2d8ffeb0a2337b8156860
Alex <alex00romagnoli@gmail.com>
2024-03-08 11:07:05 +0100

    footer scheletro

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -4,6 +4,7 @@ import { PrimeReactProvider } from "primereact/api";
 import "/node_modules/primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import { Header } from "./header/Header";
+import { Footer } from "./footer/Footer";
 
 export function Navigation() {
 	return (
@@ -11,10 +12,13 @@ export function Navigation() {
 			{/* La width  dei componenti (header, body e footer) è gestita qui, w-10 indica una width del 83.33% 
 			il width dei componenti dentro questo div si baserà sul 83.33% della viewport*/}
 			<div className="flex flex-column  m-auto justify-content-center gap-5 w-10">
+				{/* ▼ Rendering del header ▼ */}
 				<Header />
 				<Routes>
 					<Route path="/" element={<Home />}></Route>
 				</Routes>
+				{/* ▼ Rendering del footer ▼ */}
+				<Footer />
 			</div>
 		</PrimeReactProvider>
 	);
