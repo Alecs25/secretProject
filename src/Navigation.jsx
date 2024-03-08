d429dfb8152a889ba390c0d675d032c0794bad63
Alex <alex00romagnoli@gmail.com>
2024-03-08 10:04:19 +0100

    cleanup

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -8,6 +8,8 @@ import { Header } from "./header/Header";
 export function Navigation() {
 	return (
 		<PrimeReactProvider>
+			{/* La width  dei componenti (header, body e footer) è gestita qui, w-10 indica una width del 83.33% 
+			il width dei componenti dentro questo div si baserà sul 83.33% della viewport*/}
 			<div className="flex flex-column  m-auto justify-content-center gap-5 w-10">
 				<Header />
 				<Routes>
