c927c0e2219793f45bf58e2a7defb9c026a6f1e2
Alecs25 <alex00romagnoli@gmail.com>
2024-03-26 15:30:30 +0100

    DELETE metodo su backend + introduzione manager articoli

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -10,13 +10,14 @@ import { EditorProduct } from "./pages/ProductEditor";
 import { ArticlesLoader } from "./content/articles/ArticlesLoader";
 import { Login } from "./pages/Login";
 import { ArticleTemplate } from "./pages/Template/ArticleTemplate";
+import { ArticlesManager } from "./pages/ArticlesManager";
 
 export function Navigation() {
 	return (
 		<PrimeReactProvider>
 			{/* La width  dei componenti (header, body e footer) è gestita qui, w-10 indica una width del 83.33% 
 			il width dei componenti dentro questo div si baserà sul 83.33% della viewport*/}
-			<div style={{position:"relative"}} className="flex flex-column  m-auto justify-content-center gap-5">
+			<div style={{ position: "relative" }} className="flex flex-column  m-auto justify-content-center gap-5">
 				{/* ▼ Rendering del header ▼ */}
 				<Header />
 				<ArticlesLoader>
@@ -25,11 +26,12 @@ export function Navigation() {
 						<Route path="/product" element={<Product />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/editor" element={<EditorProduct />} />
-						<Route path="/article/:articleId"  element={<ArticleTemplate />} />
+						<Route path="/articlemanager" element={<ArticlesManager />} />
+						<Route path="/article/:articleId" element={<ArticleTemplate />} />
 					</Routes>
 					<Footer />
 				</ArticlesLoader>
-				</div>
+			</div>
 		</PrimeReactProvider>
 	);
 }
