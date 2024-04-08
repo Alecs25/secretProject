eb22b95b627255e96f7b1186d630e3e8d2c7d85a
Matteo <matteodurso45@gmail.com>
2024-04-08 19:25:19 +0200

    modifica card

--- src/pages/Template/ViewArticle.jsx
+++ src/pages/Template/ViewArticle.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import parse from "html-react-parser";
 import { FetchArticle } from "../../components/articles/Methods";
-
+import "./ViewArticle.css"
 export function ViewArticle() {
 	const { articleId } = useParams();
 	const [data, setData] = useState(null);
@@ -16,7 +16,7 @@ export function ViewArticle() {
 	}, [data]);
 
 	return (
-		<article className="card w-11 m-auto bg-primary text-justify flex flex-column align-items-center p-5">
+		<article className="card w-11 m-auto bg-primary text-justify flex flex-column align-items-center p-5 article-body">
 			{data && parse(data.article.title)}
 
 			{data && parse(data.article.body)}
