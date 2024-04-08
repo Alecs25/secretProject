3c6347ba33c0d44c112182fd152c4a160785b720
vitalikpatapchuk <150099283+vitalikpatapchuk@users.noreply.github.com>
2024-04-08 15:43:26 +0200

    card + stile

--- src/components/Cards/cardapi.jsx
+++ src/components/Cards/cardapi.jsx
@@ -1,30 +1,28 @@
 import useSWR from 'swr';
-
+import "../Cards/cardapi.css"
 function fetcher(url) {
     return fetch(url).then(res => res.json());
 }
 
 export function Cardgrande() {
     const { data, error } = useSWR('http://localhost:3000/api/oggetti', fetcher);
-    console.log(data)
 
     if (error) return <div>Errore nella richiesta...</div>;
     if (!data) return <div>Caricamento...</div>;
     if (!data.length) return <div>Nessun dato trovato</div>;
 
     return (
-        
-            <div className="card-container123">
+        <div className="card-container123">
             {data.map((oggetto, index) => (
-                <div key={index} className="card123" style={{ backgroundImage: `url(${oggetto.immagine})` }}>
-                    <div className="card-content123">
-                        <h2 className="card-title123">{oggetto.titolo}</h2>
-                        <p className="card-description123">{oggetto.descrizione}</p>
-                        <a className="card-link123" href={oggetto.link} target="_blank" rel="noopener noreferrer"></a>
+                <a key={index} className="card-link123" href={oggetto.link} target="_blank" rel="noopener noreferrer">
+                    <div className="card123" style={{ backgroundImage: `url(${oggetto.immagine})` }}>
+                        <div className="card-content123">
+                            <h2 className="card-title123">{oggetto.titolo}</h2>
+                            <p className="card-description123">{oggetto.descrizione}</p>
+                        </div>
                     </div>
-                </div>
+                </a>
             ))}
-            </div>
-        
+        </div>
     );
 }
